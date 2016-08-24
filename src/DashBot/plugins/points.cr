1b5629e3bac920f4536bc6d8f776eeea51fbf926
Arthur Poulet <arthur.poulet@mailoo.org>
2016-08-24 15:15:07 +0200

    Add: !pl !plu for plugin Points

--- src/DashBot/plugins/points.cr
+++ src/DashBot/plugins/points.cr
@@ -5,6 +5,11 @@ module DashBot
       include Rights
 
       def bind(bot)
+        bind_add(bot)
+        bind_list(bot)
+      end
+
+      def bind_add(bot)
         bot.on("PRIVMSG", message: /^!p ([[:graph:]]+) ([[:graph:]]+)/) do |msg, match|
           match = match.as Regex::MatchData
           DB.exec "INSERT INTO points (assigned_to, assigned_by, type, created_at)
@@ -14,6 +19,20 @@ module DashBot
           msg.reply "#{match[2]} has now #{n} point#{n > 1 ? "s" : ""} #{match[1]}"
         end
       end
+
+      def bind_list(bot)
+        bot.on("PRIVMSG", message: /^!pl ([[:graph:]]+)/) do |msg, match|
+          match = match.as Regex::MatchData
+          points = DB.exec({String, Int64}, "SELECT assigned_to, COUNT(*) FROM points WHERE type = $1 GROUP BY assigned_to ORDER BY COUNT(*) DESC LIMIT 5;", [match[1]]).to_hash
+          msg.reply "#{match[1]}: " + points.map{|point| "#{point["assigned_to"]}: #{point["count"]}" }.join(", ")
+        end
+        bot.on("PRIVMSG", message: /^!plu ([[:graph:]]+)/) do |msg, match|
+          match = match.as Regex::MatchData
+          points = DB.exec({String, Int64}, "SELECT type, COUNT(*) FROM points WHERE assigned_to = $1 GROUP BY type ORDER BY COUNT(*) DESC LIMIT 5;", [match[1]]).to_hash
+          msg.reply "#{match[1]}: " + points.map{|point| "#{point["type"]}: #{point["count"]}" }.join(", ")
+        end
+      end
+      #
     end
   end
 end
