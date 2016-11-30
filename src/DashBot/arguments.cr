fa65696a585d497bd10f4ab0399ec43059ae832d
Arthur Poulet <arthur.poulet@mailoo.org>
2016-11-30 00:10:36 +0000

    Fix Arguments error when directory doesn't exist

--- src/DashBot/arguments.cr
+++ src/DashBot/arguments.cr
@@ -9,8 +9,14 @@ class DashBot::Arguments
   def use
     OptionParser.parse! do |parser|
       parser.banner = "Usage: DashBot [arguments]"
-      parser.on("-e error", "--errors=PATH", "Specifies the path of the error log") { |path| @err = path }
-      parser.on("-o output", "--output=PATH", "Specifies the path of the output log") { |path| @out = path }
+      parser.on("-e error", "--errors=PATH", "Specifies the path of the error log") { |path|
+        @err = path
+        Dir.mkdir(File.dirname(@err)) if !Dir.exists?(File.dirname(@err))
+      }
+      parser.on("-o output", "--output=PATH", "Specifies the path of the output log") { |path|
+        @out = path
+        Dir.mkdir(File.dirname(@out)) if !Dir.exists?(File.dirname(@out))
+      }
       parser.on("-h", "--help", "Show this help") { puts parser; exit }
       parser.on("-d", "--daemonize", "Run in background as daemon") { @daemonize = true }
     end
